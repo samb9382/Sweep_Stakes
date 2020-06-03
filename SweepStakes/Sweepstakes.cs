8c9bbb80854628a6f2ecfec3f2ce5f1440cae6a8
SammySupreme B <samb9382@gmail.com>
2020-06-03 09:56:01 -0500

    Created a Sweestakes class with empty methods from directed from the user story

---
+++ SweepStakes/Sweepstakes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Sweepstakes
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+        public void SweepStakes(string name)
+        {
+            
+        }
+
+        public void RegisterConsestant(Contestant contestant)
+        {
+            
+        }
+
+        public void Contestant PickWinner()
+        {
+            
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            
+        }
+    }
+}

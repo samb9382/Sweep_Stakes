31578b3de38ea4b20480433146df091db0093729
SammySupreme B <samb9382@gmail.com>
2020-06-03 09:51:08 -0500

    created a contestant class with member variables for first name last name email and registration number

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+        //member variables
+        public string firstName;
+        public string lastName;
+        public string email;
+        public int registrationNumber;
+           
+        //constructor
+
+        //member methods
+    }
+}
